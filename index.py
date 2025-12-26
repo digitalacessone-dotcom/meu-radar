f501e397853b242e58009cf6a34a0280f51c7179
digitalacessone-dotcom <digitalacessone@gmail.com>
2025-12-26 19:02:33 -0300

    Update index.py

--- index.py
+++ index.py
@@ -525,7 +525,7 @@ def index():
             if(!act || !pos) return;
             const planeElement = document.getElementById('arr');
             const bearingToPlane = calculateBearing(pos.lat, pos.lon, act.lat, act.lon);
-            const finalRotation = (bearingToPlane - deviceHeading - 60);
+            const finalRotation = (bearingToPlane - deviceHeading - 10);
             planeElement.style.transform = `rotate(${finalRotation}deg)`;
         }
 
